80c54a1860dc34dbb8a33c08a94a39dae48788af
Preston Swigart <prestonlswigart@gmail.com>
2022-09-29 10:42:48 -0400

    9.29.22 10:42

--- src/SimpleIOMath.java
+++ src/SimpleIOMath.java
@@ -15,12 +15,19 @@ public class SimpleIOMath {
      */
     public void promptUser() {
         Scanner in = new Scanner(System.in);
+        System.out.println("* Sit yourself down, take a seat *");
+        System.out.println("* All you gotta do is repeat after me *");
         System.out.print("Enter Your Name: ");
         name = in.nextLine();
         System.out.print("Enter Your Age: ");
         age = in.nextInt();
         System.out.print("Enter Your Favorite Number: ");
         favNumber = in.nextInt();
+        System.out.println("I'm gonna teach you how to sing it out");
+        System.out.println("Come on, come on, come on");
+        System.out.println("Let me tell you what it's all about");
+        System.out.println("Reading, writing, arithmetic");
+        System.out.println("Are the branches of the learning tree");
     }
 
     /**
@@ -29,17 +36,17 @@ public class SimpleIOMath {
     public void printInfo(){
         System.out.println("Your name is: " + name);
         System.out.println("Your age is " + age);
-        //System.out.println("At your next birthday, you will be " + Variable);
+        System.out.println("At your next birthday, you will be " + (age + 1));
         //System.out.println("The first prime factor of " + age + "is " + Variable);
         System.out.println("Your favorite number is: " + favNumber);
-        //System.out.println("Your favorite number squared is: " + Variable);
+        System.out.println("Your favorite number squared is: " + favNumber * favNumber);
 
     }
 
     /**
      *
      * @param num
-     * @return
+     * @retur
      */
     private boolean isPrime(int num) {
         for(int i = 2; i <= Math.sqrt(num)+1; i++) {
